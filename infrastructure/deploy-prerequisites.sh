6aa6d311a9ec8f4ad205328f35fc2ecd9555ff72
Mike Roberts <mike@symphonia.io>
2019-01-26 17:55:30 -0500

    Adding 4 EU regions

--- infrastructure/deploy-prerequisites.sh
+++ infrastructure/deploy-prerequisites.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for region in us-east-1 us-east-2 us-west-1 us-west-2
+for region in eu-central-1 eu-west-1 eu-west-2 eu-west-3
 do
-  aws cloudformation create-stack --region $region --stack-name lambda-benchmarking-prerequisites --template-body file://pre-region-prerequisites.yaml
+  aws cloudformation create-stack --region $region --stack-name lambda-benchmarking-prerequisites --template-body file://per-region-prerequisites.yaml
 done
--- node8-codebuild.sh
+++ node8-codebuild.sh
@@ -3,7 +3,11 @@
 if  [ -z "$US_EAST_1_ARTIFACT_BUCKET" ] || 
     [ -z "$US_EAST_2_ARTIFACT_BUCKET" ] ||
     [ -z "$US_WEST_1_ARTIFACT_BUCKET" ] ||
-    [ -z "$US_WEST_2_ARTIFACT_BUCKET" ]
+    [ -z "$US_WEST_2_ARTIFACT_BUCKET" ] ||
+    [ -z "$EU_CENTRAL_1_ARTIFACT_BUCKET" ] ||
+    [ -z "$EU_WEST_1_ARTIFACT_BUCKET" ] ||
+    [ -z "$EU_WEST_2_ARTIFACT_BUCKET" ] ||
+    [ -z "$EU_WEST_3_ARTIFACT_BUCKET" ]
 then
       echo "Artifact bucket environment variables are not set correctly"
       exit 1
@@ -26,6 +30,10 @@ sam_package us-east-1 ${US_EAST_1_ARTIFACT_BUCKET} node8-generator
 sam_package us-east-2 ${US_EAST_2_ARTIFACT_BUCKET} node8-generator
 sam_package us-west-1 ${US_WEST_1_ARTIFACT_BUCKET} node8-generator
 sam_package us-west-2 ${US_WEST_2_ARTIFACT_BUCKET} node8-generator
+sam_package eu-central-1 ${EU_CENTRAL_1_ARTIFACT_BUCKET} node8-generator
+sam_package eu-west-1 ${EU_WEST_1_ARTIFACT_BUCKET} node8-generator
+sam_package eu-west-2 ${EU_WEST_2_ARTIFACT_BUCKET} node8-generator
+sam_package eu-west-3 ${EU_WEST_3_ARTIFACT_BUCKET} node8-generator
 
 echo "** Starting packaging of Timings Collector"
 cd ../../timingsCollector
