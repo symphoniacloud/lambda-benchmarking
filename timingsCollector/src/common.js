5134d7034f42556ba130faa28ec513c748ac7e90
Mike Roberts <mike@symphonia.io>
2019-02-06 19:12:31 -0500

    Fix to region list

--- timingsCollector/src/common.js
+++ timingsCollector/src/common.js
@@ -3,7 +3,7 @@ exports.flatten = function (arrayOfArrays) {
 }
 
 exports.allRegions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2",
-                      "eu-central-1", "eu-west-2", "eu-west-2", "eu-west-3"];
+                      "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3"];
 
 exports.getAndCheckEnvVar = function getAndCheckEnvVar(envVarName) {
   const envVarValue = process.env[envVarName];
