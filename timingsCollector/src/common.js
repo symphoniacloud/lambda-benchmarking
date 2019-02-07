f08c15077b747c89e4f20c7c37508f28541dc39e
Mike Roberts <mike@symphonia.io>
2019-02-06 19:20:09 -0500

    Fixing - X-Ray not available in Paris

--- timingsCollector/src/common.js
+++ timingsCollector/src/common.js
@@ -2,8 +2,9 @@ exports.flatten = function (arrayOfArrays) {
   return [].concat(...arrayOfArrays);
 }
 
+// X-Ray not currently available in eu-west-3 / Paris
 exports.allRegions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2",
-                      "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3"];
+                      "eu-central-1", "eu-west-1", "eu-west-2"];
 
 exports.getAndCheckEnvVar = function getAndCheckEnvVar(envVarName) {
   const envVarValue = process.env[envVarName];
